bbe6c87dc379a6e649ff36941f2848e8255e18fa
zuroyu <zuroyu@gmail.com>
2012-12-17 17:17:57 +0800

    get google search data

---
+++ key.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import urllib2,urllib
+import simplejson
+
+
+#q：搜索内容
+#rsz：每页显示条数
+#start：第几页
+
+seachstr = 'china wholesalers'
+
+for x in range(6):
+   # print "page:%s"%(x+1)
+    page = x * 8
+    psize = 8
+    url =('https://ajax.googleapis.com/ajax/services/search/web'
+                  '?v=1.0&q=%s&rsz=%s&start=%s') %(urllib.quote(seachstr),psize,page)
+    try:
+        request = urllib2.Request(
+        url, None, {'Referer': 'http://www.sina.com'})
+        response = urllib2.urlopen(request)
+
+        # Process the JSON string.
+        #得到json的数据格式，利用json在线校验器可以得到清晰的样式。
+        results = simplejson.load(response)
+        #从results中取出results
+        infoaaa = results['responseData']['results']
+    except Exception,e:
+        print e
+    else:
+        #定义Google Rank
+        n = psize*x
+        for minfo in infoaaa:
+            n += 1
+            #从infoaaa中取出url链接
+            print n
+            print minfo['url']
+            
