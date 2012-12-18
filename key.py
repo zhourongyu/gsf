1ee2c9c2eecf40f4cf2c51aa34180544975e8e4c
zuroyu <zuroyu@gmail.com>
2012-12-18 12:31:56 +0800

    new :

--- key.py
+++ key.py
@@ -2,25 +2,26 @@
 # -*- coding: utf-8 -*-
 import urllib2,urllib
 import simplejson
-
-
 #q：搜索内容
 #rsz：每页显示条数
 #start：第几页
 
-seachstr = 'china wholesalers'
-
-for x in range(6):
+seachstr = 'china'
+psize = 8
+for x in range(15):
    # print "page:%s"%(x+1)
-    page = x * 8
-    psize = 8
+    if(x==0):
+        page = 1
+    else:
+        page = x * psize
+
     url =('https://ajax.googleapis.com/ajax/services/search/web'
                   '?v=1.0&q=%s&rsz=%s&start=%s') %(urllib.quote(seachstr),psize,page)
     try:
         request = urllib2.Request(
         url, None, {'Referer': 'http://www.sina.com'})
-        response = urllib2.urlopen(request)
 
+        response = urllib2.urlopen(request)
         # Process the JSON string.
         #得到json的数据格式，利用json在线校验器可以得到清晰的样式。
         results = simplejson.load(response)
@@ -28,6 +29,7 @@ for x in range(6):
         infoaaa = results['responseData']['results']
     except Exception,e:
         print e
+        pass
     else:
         #定义Google Rank
         n = psize*x
